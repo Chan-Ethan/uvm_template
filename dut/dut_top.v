a1423f5a932a16eab98137be5b933d9b5f83c2e3
Ethan_PC <wsqlbh@163.com>
2025-01-21 10:35:32 +0000

    [tmp]add driver

--- dut/dut_top.v
+++ dut/dut_top.v
@@ -1,7 +1,20 @@
 module dut_top (
-    input   clk
+    input               clk,
+    input               rst_n,
+    input       [7: 0]  rxd,
+    input               rx_dv,
+    output reg  [7: 0]  txd,
+    output reg          tx_en
 );
-    initial begin
-        $display("dut_top module");
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            txd <= 8'b0;
+            tx_en <= 1'b0;
+        end
+        else begin
+            txd <= rxd;
+            tx_en <= rx_dv;
+        end
     end
+
 endmodule
