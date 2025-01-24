4d2625d53d2016f7c9afa2ea517baf64aafc2ee7
Ethan_PC <wsqlbh@163.com>
2025-01-24 21:16:45 +0000

    updata flist

--- top/flist.f
+++ top/flist.f
@@ -4,9 +4,12 @@
 // UVM platform
 ../../my_port/my_if.sv
 ../../my_port/my_transaction.sv
+../../my_port/my_sequence.sv
+../../my_port/my_sequencer.sv
 ../../my_port/my_monitor.sv
 ../../my_port/my_driver.sv
 ../../my_port/my_agent.sv
+
 ../../model/my_model.sv
 ../../scoreboard/my_scoreboard.sv
 ../../env/my_env.sv
